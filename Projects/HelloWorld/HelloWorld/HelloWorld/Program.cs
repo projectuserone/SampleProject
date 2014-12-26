856cac23c4d96ebbe7b272fa1b99044ad3e5661e
Param <mailmebreddy@gmail.com>
2014-12-27 00:58:03 +0530

    Code Modification and new project added

--- Projects/HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ Projects/HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,10 +7,15 @@ namespace HelloWorld
 {
     class Program
     {
+        /// <summary>
+        /// Programm Main comments updated
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
             Console.Read();
+
         }
     }
 }
---
+++ Projects/HelloWorld/HelloWorld/HelloWorldLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorldLib
+{
+    public class Class1
+    {
+        Class1 obj = new Class1();
+    }
+}
